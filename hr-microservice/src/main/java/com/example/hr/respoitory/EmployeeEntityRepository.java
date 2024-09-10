2ee7cf7aee87157b2bd15bcb444f3af6da4328b7
Binnur KURT <deepcloudlabs@gmail.com>
2024-09-10 16:33:47 +0300

    initial commit

--- hr-microservice/src/main/java/com/example/hr/respoitory/EmployeeEntityRepository.java
+++ hr-microservice/src/main/java/com/example/hr/respoitory/EmployeeEntityRepository.java
@@ -3,6 +3,7 @@ package com.example.hr.respoitory;
 import java.util.Collection;
 import java.util.List;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,12 +12,12 @@ import com.example.hr.entity.EmployeeEntity;
 
 public interface EmployeeEntityRepository extends JpaRepository<EmployeeEntity, String>{
 	Collection<EmployeeEntity> findFirst10ByLastName(String lastname);
-	@Query(value = "select e from EmployeeEntity e where e.lastName=:lastName limit 10")
-	Collection<EmployeeEntity> getir(String lastname);
+	@Query(value = "select e from EmployeeEntity e where e.lastName=:lastName")
+	Collection<EmployeeEntity> getir(PageRequest page,String lastName);
 	
 	EmployeeEntity findTopByOrderByBirthYearDesc();
 	EmployeeEntity findTopByOrderByBirthYearAsc();
 	EmployeeEntity findTopByOrderBySalaryDesc();
 	
-	List<EmployeeEntity> findByDepartmentIn(List<Department> departments);
+	List<EmployeeEntity> findByDepartmentsIn(List<Department> departments);
 }
